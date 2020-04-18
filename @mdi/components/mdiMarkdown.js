cdb12ee85989b13392321016e62ddcafe54e7eb5
Bot <git@templarian.com>
2020-04-18 03:15:27 +0000

    Deploy @mdi/components

--- @mdi/components/mdiMarkdown.js
+++ @mdi/components/mdiMarkdown.js
@@ -1,4 +1,4 @@
-var mdiSearch = (function () {
+var mdiMarkdown = (function () {
     'use strict';
 
     /*! *****************************************************************************
@@ -135,64 +135,30 @@ var mdiSearch = (function () {
             });
         };
     }
-    function Part() {
-        return function (target, propertyKey, descriptor) {
-            Object.defineProperty(target, propertyKey, {
-                get: function () {
-                    var _a;
-                    var key = propertyKey.replace(/^\$/, '');
-                    return (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.querySelector("[part~=" + key + "]");
-                }
-            });
-        };
-    }
 
-    var template$1 = "<div>\n  <input part=\"input\" type=\"text\" />\n  <svg viewBox=\"0 0 24 24\"><path d=\"M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z\" /></svg>\n  <ul part=\"list\">\n    <li>Guides</li>\n    <li>Angular</li>\n    <li>Icons</li>\n    <li>angular</li>\n  </ul>\n</div>";
+    var template$1 = "<h1>Header 1</h1>\n\n<p>Paragraph</p>\n\n<h2>Header 2</h2>\n\n<p>Paragraph</p>\n\n<h3>Header 3</h3>\n\n<p>Paragraph</p>\n\n<blockquote>\n  <p>Blockquote</p>\n</blockquote>";
 
-    var style$1 = ":host {\n  align-self: center;\n  width: 10rem;\n}\n\ndiv {\n  display: grid;\n  grid-template-columns: 1fr 0;\n  grid-template-rows: 1fr 0;\n}\ninput {\n  grid-row: 1;\n  grid-column: 1;\n  border-radius: 0.25rem;\n  border: 0;\n  padding: 0.25rem 0.5rem;\n  font-size: 1rem;\n  outline: none;\n  width: calc(100% - 1rem);\n}\ninput:focus + svg path {\n  fill: #ccc;\n}\nsvg {\n  grid-row: 1;\n  grid-column: 2;\n  width: 1.5rem;\n  height: 1.5rem;\n  justify-self: right;\n  margin-right: 0.25rem;\n  pointer-events: none;\n  align-self: center;\n}\nsvg > path {\n  fill: #555;\n  transition: fill 0.3s ease-in-out;\n}\nul {\n  display: none;\n  background: #FFF;\n  grid-row: 2;\n  grid-column: 1 / span 2;\n  list-style: none;\n  padding: 0;\n  margin: 0;\n}\nul > li {\n  background: #FFF;\n  color: #222;\n  padding: 0.25rem 0.5rem;\n  border: 1px solid #DDD;\n}";
+    var style$1 = ":host {\n  color: #453C4F;\n}\n\nblockquote {\n  border-left: 4px solid #453C4F;\n  padding: 0 0.5rem;\n  margin: 0;\n}";
 
-    const noIcon = 'M0 0h24v24H0V0zm2 2v20h20V2H2z';
-    let MdiSearch = class MdiSearch extends HTMLElement {
+    let MdiMarkdown = class MdiMarkdown extends HTMLElement {
         constructor() {
             super(...arguments);
-            this.path = noIcon;
-            this.isOpen = false;
-        }
-        connectedCallback() {
-            this.$input.addEventListener('input', this.handleInput.bind(this));
-            this.$input.addEventListener('focus', this.handleFocus.bind(this));
-        }
-        handleInput(e) {
-            const target = e.target;
-            const { value } = target;
-            console.log(value);
-        }
-        handleFocus() {
-            this.isOpen = true;
-            this.$list.style.display = 'block';
-        }
-        render() {
+            this.text = '';
         }
     };
     __decorate([
         Prop()
-    ], MdiSearch.prototype, "path", void 0);
-    __decorate([
-        Part()
-    ], MdiSearch.prototype, "$input", void 0);
-    __decorate([
-        Part()
-    ], MdiSearch.prototype, "$list", void 0);
-    MdiSearch = __decorate([
+    ], MdiMarkdown.prototype, "text", void 0);
+    MdiMarkdown = __decorate([
         Component({
-            selector: 'mdi-search',
+            selector: 'mdi-markdown',
             style: style$1,
             template: template$1
         })
-    ], MdiSearch);
-    var MdiSearch$1 = MdiSearch;
+    ], MdiMarkdown);
+    var MdiMarkdown$1 = MdiMarkdown;
 
-    return MdiSearch$1;
+    return MdiMarkdown$1;
 
 }());
-//# sourceMappingURL=mdiSearch.js.map
+//# sourceMappingURL=mdiMarkdown.js.map
