5c1c538dc475501806dfb139a9320e3fb6ee1d5d
Bot <git@templarian.com>
2020-04-25 20:09:03 +0000

    Deploy @mdi/components

---
+++ @mdi/components/mdiInputHexRgb.js
@@ -0,0 +1,192 @@
+var mdiInputHexRgb = (function () {
+    'use strict';
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+    this file except in compliance with the License. You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0
+
+    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+    MERCHANTABLITY OR NON-INFRINGEMENT.
+
+    See the Apache Version 2.0 License for specific language governing permissions
+    and limitations under the License.
+    ***************************************************************************** */
+
+    function __decorate(decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    }
+
+    var init = Symbol('init');
+    var template = Symbol('template');
+    var style = Symbol('style');
+    var parent = Symbol('parent');
+    function extendTemplate(base, append) {
+        if (append && append.match(/<parent\/>/)) {
+            return append.replace(/<parent\/>/, base);
+        }
+        else {
+            return "" + base + (append || '');
+        }
+    }
+    function Component(config) {
+        return function (cls) {
+            if (cls.prototype[parent]) {
+                cls.prototype[parent].push(cls.prototype);
+                cls.prototype[style] = "" + cls.prototype[style] + config.style;
+                cls.prototype[template] = extendTemplate(cls.prototype[template], config.template || null);
+            }
+            else {
+                cls.prototype[parent] = [cls.prototype];
+                cls.prototype[style] = config.style || '';
+                cls.prototype[template] = config.template || '';
+            }
+            var connectedCallback = cls.prototype.connectedCallback || (function () { });
+            var disconnectedCallback = cls.prototype.disconnectedCallback || (function () { });
+            cls.prototype.connectedCallback = function () {
+                var _this = this;
+                if (!this[init] && config.template) {
+                    var $template = document.createElement('template');
+                    $template.innerHTML = cls.prototype[template] + "<style>" + cls.prototype[style] + "</style>";
+                    var $node = document.importNode($template.content, true);
+                    if (config.useShadow === false) {
+                        this.appendChild($node);
+                    }
+                    else {
+                        this.attachShadow({ mode: 'open' }).appendChild($node);
+                    }
+                }
+                else if (this[init] && config.style) ;
+                else if (this[init] && config.template) {
+                    throw new Error('template from base class cannot be overriden. Fix: remove template from @Component');
+                }
+                else if (config.template) {
+                    throw new Error('You need to pass a template for the element');
+                }
+                if (this.componentWillMount) {
+                    this.componentWillMount();
+                }
+                this[parent].map(function (p) {
+                    if (p.render) {
+                        p.render.call(_this);
+                    }
+                });
+                this[init] = true;
+                connectedCallback.call(this);
+                if (this.componentDidMount) {
+                    this.componentDidMount();
+                }
+            };
+            cls.prototype.disconnectedCallback = function () {
+                if (this.componentWillUnmount) {
+                    this.componentWillUnmount();
+                }
+                disconnectedCallback.call(this);
+                if (this.componentDidUnmount) {
+                    this.componentDidUnmount();
+                }
+            };
+            cls.prototype.attributeChangedCallback = function (name, oldValue, newValue) {
+                this[name] = newValue;
+                // if (this.attributeChangedCallback) {
+                // this.attributeChangedCallback(name, oldValue, newValue);
+                // }
+            };
+            if (!window.customElements.get(config.selector)) {
+                window.customElements.define(config.selector, cls);
+            }
+        };
+    }
+    function Prop() {
+        return function (target, propertyKey, descriptor) {
+            var constructor = target.constructor;
+            if (!constructor.observedAttributes) {
+                constructor.observedAttributes = [];
+            }
+            var observedAttributes = constructor.observedAttributes;
+            if (!constructor.symbols) {
+                constructor.symbols = {};
+            }
+            var symbols = constructor.symbols;
+            observedAttributes.push(propertyKey);
+            var symbol = Symbol(propertyKey);
+            symbols[propertyKey] = symbol;
+            Object.defineProperty(target, propertyKey, {
+                get: function () {
+                    return this[symbol];
+                },
+                set: function (value) {
+                    var _this = this;
+                    this[symbol] = value;
+                    if (this[init]) {
+                        this[parent].map(function (p) {
+                            if (p.render) {
+                                p.render.call(_this);
+                            }
+                        });
+                    }
+                }
+            });
+        };
+    }
+    function Part() {
+        return function (target, propertyKey, descriptor) {
+            Object.defineProperty(target, propertyKey, {
+                get: function () {
+                    var _a;
+                    var key = propertyKey.replace(/^\$/, '');
+                    return (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.querySelector("[part~=" + key + "]");
+                }
+            });
+        };
+    }
+
+    var template$1 = "<div>\n  <input part=\"hex\" type=\"text\" />\n  <label part=\"labelRed\">R</label>\n  <input part=\"red\" type=\"number\" step=\"1\" min=\"0\" max=\"255\" />\n  <label part=\"labelGreen\">G</label>\n  <input part=\"green\" type=\"number\" step=\"1\" min=\"0\" max=\"255\" />\n  <label part=\"labelBlue\">B</label>\n  <input part=\"blue\" type=\"number\" step=\"1\" min=\"0\" max=\"255\" />\n</div>";
+
+    var style$1 = "div {\n  display: grid;\n  grid-template-rows: auto 1rem 2rem 1rem 2rem 1rem 2rem;\n  grid-template-rows: 1fr;\n}\n\n[part~=\"hex\"] {\n  grid-row: 1;\n  grid-column: 1;\n}\n\n[part~=\"labelRed\"] {\n  grid-row: 1;\n  grid-column: 2;\n  background: red;\n}\n\n[part~=\"red\"] {\n  grid-row: 1;\n  grid-column: 3;\n}\n\n[part~=\"labelGreen\"] {\n  grid-row: 1;\n  grid-column: 4;\n  background: green;\n  color: white;\n}\n\n[part~=\"green\"] {\n  grid-row: 1;\n  grid-column: 5;\n}\n\n[part~=\"labelBlue\"] {\n  grid-row: 1;\n  grid-column: 6;\n  background: blue;\n  color: white;\n}\n\n[part~=\"blue\"] {\n  grid-row: 1;\n  grid-column: 7;\n}\n\n[part~=\"labelRed\"],\n[part~=\"labelGreen\"],\n[part~=\"labelBlue\"] {\n  display: flex;\n  margin-left: 0.25rem;\n  align-items: center;\n  justify-content: center;\n  border-radius: 0.25rem 0 0 0.25rem;\n  color: white;\n  min-width: 1rem;\n}\n\n[part~=\"hex\"] {\n  border-radius: 0.25rem;\n}\n\n[part~=\"hex\"],\n[part~=\"red\"],\n[part~=\"green\"],\n[part~=\"blue\"] {\n  outline: none;\n  font-size: 1rem;\n  padding: 0.25rem 0.5rem;\n  border: 1px solid #453C4F;\n}\n\n[part~=\"red\"],\n[part~=\"green\"],\n[part~=\"blue\"] {\n  border-radius: 0 0.25rem 0.25rem 0;\n  -moz-appearance: textfield;\n  width: 3rem;\n  width: calc(100% - 1rem);\n}\n\n[part~=\"red\"]::-webkit-inner-spin-button,\n[part~=\"red\"]::-webkit-outer-spin-button,\n[part~=\"green\"]::-webkit-inner-spin-button,\n[part~=\"green\"]::-webkit-outer-spin-button,\n[part~=\"blue\"]::-webkit-inner-spin-button,\n[part~=\"blue\"]::-webkit-outer-spin-button {\n  -webkit-appearance: none;\n  margin: 0;\n}";
+
+    let MdiNav = class MdiNav extends HTMLElement {
+        constructor() {
+            super(...arguments);
+            this.value = '#000000';
+        }
+        connectedCallback() {
+            this.$hex.value = this.value;
+        }
+        render() {
+        }
+    };
+    __decorate([
+        Prop()
+    ], MdiNav.prototype, "value", void 0);
+    __decorate([
+        Part()
+    ], MdiNav.prototype, "$hex", void 0);
+    __decorate([
+        Part()
+    ], MdiNav.prototype, "$red", void 0);
+    __decorate([
+        Part()
+    ], MdiNav.prototype, "$green", void 0);
+    __decorate([
+        Part()
+    ], MdiNav.prototype, "$blue", void 0);
+    MdiNav = __decorate([
+        Component({
+            selector: 'mdi-input-hex-rgb',
+            style: style$1,
+            template: template$1
+        })
+    ], MdiNav);
+    var MdiNav$1 = MdiNav;
+
+    return MdiNav$1;
+
+}());
+//# sourceMappingURL=mdiInputHexRgb.js.map
---
+++ @mdi/components/mdiInputRange.js
@@ -0,0 +1,191 @@
+var mdiInputRange = (function () {
+    'use strict';
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+    this file except in compliance with the License. You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0
+
+    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+    MERCHANTABLITY OR NON-INFRINGEMENT.
+
+    See the Apache Version 2.0 License for specific language governing permissions
+    and limitations under the License.
+    ***************************************************************************** */
+
+    function __decorate(decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    }
+
+    var init = Symbol('init');
+    var template = Symbol('template');
+    var style = Symbol('style');
+    var parent = Symbol('parent');
+    function extendTemplate(base, append) {
+        if (append && append.match(/<parent\/>/)) {
+            return append.replace(/<parent\/>/, base);
+        }
+        else {
+            return "" + base + (append || '');
+        }
+    }
+    function Component(config) {
+        return function (cls) {
+            if (cls.prototype[parent]) {
+                cls.prototype[parent].push(cls.prototype);
+                cls.prototype[style] = "" + cls.prototype[style] + config.style;
+                cls.prototype[template] = extendTemplate(cls.prototype[template], config.template || null);
+            }
+            else {
+                cls.prototype[parent] = [cls.prototype];
+                cls.prototype[style] = config.style || '';
+                cls.prototype[template] = config.template || '';
+            }
+            var connectedCallback = cls.prototype.connectedCallback || (function () { });
+            var disconnectedCallback = cls.prototype.disconnectedCallback || (function () { });
+            cls.prototype.connectedCallback = function () {
+                var _this = this;
+                if (!this[init] && config.template) {
+                    var $template = document.createElement('template');
+                    $template.innerHTML = cls.prototype[template] + "<style>" + cls.prototype[style] + "</style>";
+                    var $node = document.importNode($template.content, true);
+                    if (config.useShadow === false) {
+                        this.appendChild($node);
+                    }
+                    else {
+                        this.attachShadow({ mode: 'open' }).appendChild($node);
+                    }
+                }
+                else if (this[init] && config.style) ;
+                else if (this[init] && config.template) {
+                    throw new Error('template from base class cannot be overriden. Fix: remove template from @Component');
+                }
+                else if (config.template) {
+                    throw new Error('You need to pass a template for the element');
+                }
+                if (this.componentWillMount) {
+                    this.componentWillMount();
+                }
+                this[parent].map(function (p) {
+                    if (p.render) {
+                        p.render.call(_this);
+                    }
+                });
+                this[init] = true;
+                connectedCallback.call(this);
+                if (this.componentDidMount) {
+                    this.componentDidMount();
+                }
+            };
+            cls.prototype.disconnectedCallback = function () {
+                if (this.componentWillUnmount) {
+                    this.componentWillUnmount();
+                }
+                disconnectedCallback.call(this);
+                if (this.componentDidUnmount) {
+                    this.componentDidUnmount();
+                }
+            };
+            cls.prototype.attributeChangedCallback = function (name, oldValue, newValue) {
+                this[name] = newValue;
+                // if (this.attributeChangedCallback) {
+                // this.attributeChangedCallback(name, oldValue, newValue);
+                // }
+            };
+            if (!window.customElements.get(config.selector)) {
+                window.customElements.define(config.selector, cls);
+            }
+        };
+    }
+    function Prop() {
+        return function (target, propertyKey, descriptor) {
+            var constructor = target.constructor;
+            if (!constructor.observedAttributes) {
+                constructor.observedAttributes = [];
+            }
+            var observedAttributes = constructor.observedAttributes;
+            if (!constructor.symbols) {
+                constructor.symbols = {};
+            }
+            var symbols = constructor.symbols;
+            observedAttributes.push(propertyKey);
+            var symbol = Symbol(propertyKey);
+            symbols[propertyKey] = symbol;
+            Object.defineProperty(target, propertyKey, {
+                get: function () {
+                    return this[symbol];
+                },
+                set: function (value) {
+                    var _this = this;
+                    this[symbol] = value;
+                    if (this[init]) {
+                        this[parent].map(function (p) {
+                            if (p.render) {
+                                p.render.call(_this);
+                            }
+                        });
+                    }
+                }
+            });
+        };
+    }
+    function Part() {
+        return function (target, propertyKey, descriptor) {
+            Object.defineProperty(target, propertyKey, {
+                get: function () {
+                    var _a;
+                    var key = propertyKey.replace(/^\$/, '');
+                    return (_a = this.shadowRoot) === null || _a === void 0 ? void 0 : _a.querySelector("[part~=" + key + "]");
+                }
+            });
+        };
+    }
+
+    var template$1 = "<input part=\"input\" type=\"range\" />";
+
+    var style$1 = "";
+
+    let MdiNav = class MdiNav extends HTMLElement {
+        constructor() {
+            super(...arguments);
+            this.min = '0';
+            this.max = '100';
+            this.step = '1';
+        }
+        render() {
+            this.$input.min = this.min;
+            this.$input.max = this.max;
+            this.$input.step = this.step;
+        }
+    };
+    __decorate([
+        Prop()
+    ], MdiNav.prototype, "min", void 0);
+    __decorate([
+        Prop()
+    ], MdiNav.prototype, "max", void 0);
+    __decorate([
+        Prop()
+    ], MdiNav.prototype, "step", void 0);
+    __decorate([
+        Part()
+    ], MdiNav.prototype, "$input", void 0);
+    MdiNav = __decorate([
+        Component({
+            selector: 'mdi-input-range',
+            style: style$1,
+            template: template$1
+        })
+    ], MdiNav);
+    var MdiNav$1 = MdiNav;
+
+    return MdiNav$1;
+
+}());
+//# sourceMappingURL=mdiInputRange.js.map
